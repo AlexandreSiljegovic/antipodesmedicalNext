2505eb37569727c318d54c5abc6f605306bcbb24
Alexandre Siljegovic <alexandresiljegovic22@gmail.com>
2024-06-05 18:46:36 +0200

    css responsive carousel

--- app/components/TechnicalPlate.js
+++ app/components/TechnicalPlate.js
@@ -1,32 +1,78 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import des styles de la librairie
 
 const TechnicalPlate = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const featureCards = [
+    <div key="1" className="feature">
+      <span>01</span>
+      <h3>Radiographie rétro-alvéolaire numérique</h3>
+      <p>
+        Vitae volutpat commodo velit vel donec mus nisl eleifend tincidunt. Scelerisque et et pellentesque arcu bibendum...
+      </p>
+    </div>,
+    <div key="2" className="feature-1">
+      <div className="feature-image">
+        <Image alt='teeth' src={'/images/teeth.svg'} width={40} height={40} />
+      </div>
+      <p>Empreintes optiques par caméra</p>
+    </div>,
+    <div key="3" className="feature-2">
+      <div className="feature-image">
+        <Image alt='teeth' src={'/images/teeth.svg'} width={40} height={40} />
+      </div>
+      <p>Loupes Binoculaires</p>
+    </div>
+  ];
+
   return (
     <section className="technical-plate">
-        <div className='bg-technique'>
-            <div className='text1'>
-      <h2>Un plateau technique de pointe</h2>
-      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="1" viewBox="0 0 40 1" fill="none">
-  <path d="M0 0.5H39.5" stroke="#4F7D79"/>
-</svg>
-      <p>
-        Vitae volutpat commodo velit vel donec mus nisl eleifend tincidunt. Scelerisque et et pellentesque arcu bibendum eleifend elementum nec rutrum consectetur. Ultrices eleifend elementum nec rutrum consectetur...
-      </p>
-      <button><span>En savoir plus →</span></button>
-      </div>
-      
-      </div>
-      <div className="features">
-        <div className="feature">
-          <span>01</span>
-          <h3>Radiographie rétro-alvéolaire numérique</h3>
+      <div className='bg-technique'>
+        <div className='text1'>
+          <h2>Un plateau technique de pointe</h2>
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="1" viewBox="0 0 40 1" fill="none">
+            <path d="M0 0.5H39.5" stroke="#4F7D79"/>
+          </svg>
           <p>
-            Vitae volutpat commodo velit vel donec mus nisl eleifend tincidunt. Scelerisque et et pellentesque arcu bibendum...
+            Vitae volutpat commodo velit vel donec mus nisl eleifend tincidunt. Scelerisque et et pellentesque arcu bibendum eleifend elementum nec rutrum consectetur. Ultrices eleifend elementum nec rutrum consectetur...
           </p>
-          {/* <Image src="/images/radiography.png" alt="Radiography" width={200} height={150} /> */}
+          <button><span>En savoir plus →</span></button>
         </div>
-        <div className="feature-1"> <Image alt='teeth' src={'/images/teeth.svg'} width={40} height={40}/>Empreintes optiques par caméra</div>
-        <div className="feature-2"> <Image alt='teeth' src={'/images/teeth.svg'} width={40} height={40}/>Loupes Binoculaires</div>
+      </div>
+      <div className={`features ${isMobile ? 'carousel-features' : ''}`}>
+        {isMobile ? (
+          <Carousel  showArrows={true}
+          showStatus={false}
+          showIndicators={true}
+          infiniteLoop={true}
+          interval={3000}
+          transitionTime={500}
+          swipeable={true}
+          idth="200px" 
+          height="200px"
+          >
+            {featureCards}
+          </Carousel>
+        ) : (
+          featureCards
+        )}
       </div>
     </section>
   );
